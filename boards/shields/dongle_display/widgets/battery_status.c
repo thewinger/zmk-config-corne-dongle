f17ee925dac3bb08b458286b8be184560a6cfdd8
thewinger <winger.87@gmail.com>
2024-10-04 00:57:22 +0200

    updates display

--- boards/shields/dongle_display/widgets/battery_status.c
+++ boards/shields/dongle_display/widgets/battery_status.c
@@ -111,8 +111,8 @@ int zmk_widget_peripheral_battery_status_init(struct zmk_widget_peripheral_batte
 				     LV_IMG_CF_TRUE_COLOR);
 
 		if (i == 0) {
-			lv_obj_align(image_canvas, LV_ALIGN_TOP_LEFT, 0, 0);
-			lv_obj_align(battery_label, LV_ALIGN_TOP_LEFT, 7, 0);
+			lv_obj_align(image_canvas, LV_ALIGN_TOP_LEFT, 1, 0);
+			lv_obj_align(battery_label, LV_ALIGN_TOP_LEFT, 4, 0);
 		} else {
 			lv_obj_align(image_canvas, LV_ALIGN_TOP_RIGHT, 0, 0);
 			lv_obj_align(battery_label, LV_ALIGN_TOP_RIGHT, -7, 0);
